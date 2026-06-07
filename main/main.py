ac516be13215f10b2fe01bfb57dacbd3358cf7cc
lucas <lxpaire@inf.ufsm.br>
2026-06-07 12:08:06 -0300

    Deixando mais legivel

--- main/main.py
+++ main/main.py
@@ -11,34 +11,27 @@ from geradores.gerador import GeradorYAML
 def carregar_codigo_fonte():
     if len(sys.argv) > 1:
         caminho_arquivo = sys.argv[1]
-        if not os.path.exists(caminho_arquivo):
-            print(f"[ERRO] O arquivo '{caminho_arquivo}' não foi encontrado.")
-            return None
-            
-        print(f"Lendo código do arquivo: {caminho_arquivo}")
-        with open(caminho_arquivo, 'r', encoding='utf-8') as f:
-            return f.read()
     else:
-        print("Aviso: Nenhum arquivo passado por parâmetro. Rodando código de teste embutido...\n")
-        return """
-        AUTOMACAO "Teste Temperatura"
-        #teste comentario
-        QUANDO sensor.temperatura estiver quente
-        SE binary_sensor.porta estiver fechado
-        ENTAO notificar "Abra ás Portas!"
-        """
+        print("Aviso: Nenhum arquivo passado por parâmetro. Rodando código de teste padrão...\n")
+        caminho_arquivo = os.path.join(os.path.dirname(os.path.abspath(__file__)), "inputs", "test_padrao.homi")
+        
+    if not os.path.exists(caminho_arquivo):
+        print(f"[ERRO] O arquivo '{caminho_arquivo}' não foi encontrado.")
+        sys.exit(1)
+        
+    print(f"Lendo código do arquivo: {caminho_arquivo}")
+    with open(caminho_arquivo, 'r', encoding='utf-8') as f:
+        return f.read()
 
-def salvar_arquivo_saida(codigo_final):
-    caminho_saida = os.path.join(os.path.dirname(os.path.abspath(__file__)), "outputs", "saida.yaml")
+def salvar_arquivo_saida(codigo_final, caminho_saida):
+    os.makedirs(os.path.dirname(caminho_saida), exist_ok=True)
     with open(caminho_saida, "w", encoding="utf-8") as f:
         f.write(codigo_final)
 
 def main():
     print("--- Compilador Homi ---")
     codigo_fonte = carregar_codigo_fonte()
-    if codigo_fonte is None:
-        return
-
+    
     print("\n[FASE 1] Analisador Léxico")
     analisador_lexico = AnalisadorLexico(codigo_fonte)
     tokens_gerados = analisador_lexico.analisar()
@@ -50,8 +43,7 @@ def main():
     
     print("\n[FASE 3] Analisador Semântico")
     analisador_semantico = AnalisadorSemantico(arvore)
-    if arvore:
-        analisador_semantico.analisar()
+    analisador_semantico.analisar()
         
     todos_erros = analisador_lexico.erros + analisador_sintatico.erros + analisador_semantico.erros
     if len(todos_erros) > 0:
@@ -66,8 +58,10 @@ def main():
 
     print("\n[FASE 4] Gerador YAML")
     gerador = GeradorYAML(arvore)
+    caminho_saida = os.path.join(os.path.dirname(os.path.abspath(__file__)), "outputs", "saida.yaml")
     codigo_final = gerador.gerar()
-    salvar_arquivo_saida(codigo_final)
+    salvar_arquivo_saida(codigo_final, caminho_saida)
+    print(f"-> Arquivo YAML gerado com sucesso em: {caminho_saida}")
 
 if __name__ == "__main__":
     main()
